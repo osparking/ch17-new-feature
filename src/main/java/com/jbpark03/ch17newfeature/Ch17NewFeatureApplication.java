9f5b1969085ac0e2815655bceaa5708f5cd26dc3
osparking <jbpark03@gmail.com>
2024-01-09 10:52:40 +0900

    웹 초기자 생성 - 롬복

---
+++ src/main/java/com/jbpark03/ch17newfeature/Ch17NewFeatureApplication.java
@@ -0,0 +1,13 @@
+package com.jbpark03.ch17newfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch17NewFeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch17NewFeatureApplication.class, args);
+	}
+
+}
