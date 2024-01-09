2beee01bfc235a696b047ff52a0d734b3a961d13
osparking <jbpark03@gmail.com>
2024-01-09 12:59:31 +0900

    탑승객 명단을 포함하는 항공편 정보

---
+++ src/main/java/com/jbpark03/ch17newfeature/model/Flight.java
@@ -0,0 +1,36 @@
+package com.jbpark03.ch17newfeature.model;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import lombok.Getter;
+
+@Getter
+public class Flight {
+  private String fligheNumber;
+  private int seats;
+  private Set<Passenger> passengers = new HashSet<>();
+
+  public Flight(String fligheNumber, int seats) {
+    super();
+    this.fligheNumber = fligheNumber;
+    this.seats = seats;
+  }
+  
+  public Set<Passenger> getPassengers() {
+    return Collections.unmodifiableSet(passengers);
+  }
+
+  @Override
+  public String toString() {
+    return "Flight [fligheNumber=" + fligheNumber + "]";
+  }
+  
+  public boolean addPassenger(Passenger passenger) {
+    if (passengers.size() >= seats) {
+      throw new RuntimeException("좌석 부족으로 탑승이 거부되었습니다.");
+    }
+    return passengers.add(passenger);
+  }
+}
